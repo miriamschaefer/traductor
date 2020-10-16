eb61f063b773ed329183129dca5cad5f8144dc4b
miriamschaefer <msuarezmelian@gmail.com>
2020-10-16 21:57:30 +0200

    add posible solution

--- src/TextInput.js
+++ src/TextInput.js
@@ -5,17 +5,22 @@ class TextInput extends React.Component {
     this.textInputHandler = this.textInputHandler.bind(this);
   }
   textInputHandler(ev) {
+
+    ///\w{4,}
+    // split('').filter(() => length > 4).map(replace(/ado$/, 'ao')).join(' )
     const value = ev.target.value;
     const ado = /(ado+)(?=\s|$)/gi;
     const pal = /(para el+)(?=\s|$)/gi;
     const pa = /(para+)(?=\s|$)/gi;
     const finalS = /(s+)(?=\s|$)/gi;
+    const finalR = /(r+)(?=\s|$)/gi;
     const to = /(todo+)(?=\s|$)/gi;
 
     const result = value
 
       .replace(ado, 'ao')
       .replace(finalS, "'")
+      .replace(finalR, "'")
       .replace(pal, 'pal')
       .replace(to, "to'")
       .replace(pa, "pa'");
